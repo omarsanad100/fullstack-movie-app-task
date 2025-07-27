faab93a5f7aebca2bf352173de36a226ef0a7d2a
Omar Sanad <omarsanad.me@gmail.com>
2025-07-28 02:55:28 +0300

    refactor: update Tailwind CSS configuration and styles
    - Removed Tailwind CSS dependencies from package.json.
    - Updated App component to use Tailwind CSS classes for styling.
    - Enabled global CSS import in main.tsx.
    - Added PostCSS configuration for Tailwind CSS and Autoprefixer.
    - Created index.css to include Tailwind's base, components, and utilities.
    - Added Tailwind CSS configuration files for both frontend and src directories.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="bg-green-400">Welcome to the Fullstack Movie App</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Welcome to the Fullstack Movie App
+    </h1>
+  );
 }
 
 export default App;
