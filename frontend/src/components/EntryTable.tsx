58c545ace14945561bdf68713a5fd6f034f82386
Omar Sanad <omarsanad.me@gmail.com>
2025-07-28 20:29:51 +0300

    some enhancement

--- frontend/src/components/EntryTable.tsx
+++ frontend/src/components/EntryTable.tsx
@@ -21,8 +21,6 @@ export default function EntryTable({ onEdit, onDelete }: EntryTableProps) {
   const loader = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    console.log("Pages loaded:", data?.pages.length);
-    console.log("Has next page:", hasNextPage);
     if (!loader.current || !hasNextPage) return;
 
     const observer = new IntersectionObserver(
@@ -37,99 +35,101 @@ export default function EntryTable({ onEdit, onDelete }: EntryTableProps) {
   }, [fetchNextPage, hasNextPage]);
 
   return (
-    <div className="overflow-x-auto">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Title</TableHead>
-            <TableHead>Type</TableHead>
-            <TableHead>Director</TableHead>
-            <TableHead>Budget</TableHead>
-            <TableHead>Location</TableHead>
-            <TableHead>Duration</TableHead>
-            <TableHead>Year</TableHead>
-            <TableHead>Created At</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
+    <div className="w-full overflow-x-auto pt-6">
+      <div className="min-w-[900px] w-full">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Director</TableHead>
+              <TableHead>Budget</TableHead>
+              <TableHead>Location</TableHead>
+              <TableHead>Duration</TableHead>
+              <TableHead>Year</TableHead>
+              <TableHead>Created At</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
 
-        <TableBody>
-          {data?.pages
-            .flat()
-            .map(
-              ({
-                id,
-                title,
-                type,
-                director,
-                budget,
-                location,
-                duration,
-                year,
-                createdAt,
-              }: Entry) => (
-                <TableRow key={id}>
-                  <TableCell>{title}</TableCell>
-                  <TableCell>{type}</TableCell>
-                  <TableCell>{director}</TableCell>
-                  <TableCell>{budget}</TableCell>
-                  <TableCell>{location}</TableCell>
-                  <TableCell>{duration}</TableCell>
-                  <TableCell>{year}</TableCell>
-                  <TableCell>
-                    {new Date(createdAt).toLocaleString("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "numeric",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                  </TableCell>
-                  <TableCell className="flex gap-2">
-                    <Button
-                      className="cursor-pointer"
-                      variant="secondary"
-                      onClick={() =>
-                        onEdit({
-                          id,
-                          title,
-                          type,
-                          director,
-                          budget,
-                          location,
-                          duration,
-                          year,
-                          createdAt,
-                        })
-                      }
-                    >
-                      Edit
-                    </Button>
-                    <Button
-                      className="cursor-pointer"
-                      variant="destructive"
-                      onClick={() =>
-                        onDelete({
-                          id,
-                          title,
-                          type,
-                          director,
-                          budget,
-                          location,
-                          duration,
-                          year,
-                          createdAt,
-                        })
-                      }
-                    >
-                      Delete
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              )
-            )}
-        </TableBody>
-      </Table>
+          <TableBody>
+            {data?.pages
+              .flat()
+              .map(
+                ({
+                  id,
+                  title,
+                  type,
+                  director,
+                  budget,
+                  location,
+                  duration,
+                  year,
+                  createdAt,
+                }: Entry) => (
+                  <TableRow key={id}>
+                    <TableCell>{title}</TableCell>
+                    <TableCell>{type}</TableCell>
+                    <TableCell>{director}</TableCell>
+                    <TableCell>{budget}</TableCell>
+                    <TableCell>{location}</TableCell>
+                    <TableCell>{duration}</TableCell>
+                    <TableCell>{year}</TableCell>
+                    <TableCell>
+                      {new Date(createdAt).toLocaleString("en-US", {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </TableCell>
+                    <TableCell className="flex gap-2 flex-wrap">
+                      <Button
+                        className="cursor-pointer"
+                        variant="secondary"
+                        onClick={() =>
+                          onEdit({
+                            id,
+                            title,
+                            type,
+                            director,
+                            budget,
+                            location,
+                            duration,
+                            year,
+                            createdAt,
+                          })
+                        }
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        className="cursor-pointer"
+                        variant="destructive"
+                        onClick={() =>
+                          onDelete({
+                            id,
+                            title,
+                            type,
+                            director,
+                            budget,
+                            location,
+                            duration,
+                            year,
+                            createdAt,
+                          })
+                        }
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+          </TableBody>
+        </Table>
+      </div>
 
       <div ref={loader} className="h-8 flex items-center justify-center">
         {isFetchingNextPage && <span>Loading more...</span>}
