90b704ad2f9c52c6b661dd38be746d88de1a2187
Omar Sanad <omarsanad.me@gmail.com>
2025-07-28 19:34:34 +0300

    enhancing some logics

--- frontend/src/components/EntryTable.tsx
+++ frontend/src/components/EntryTable.tsx
@@ -1,6 +1,15 @@
 import { useEntries } from "../hooks/useEntries";
 import { useRef, useEffect } from "react";
 import type { Entry } from "./types/entry";
+import { Button } from "./UI/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./UI/table";
 
 interface EntryTableProps {
   onEdit: (entry: Entry) => void;
@@ -12,70 +21,116 @@ export default function EntryTable({ onEdit, onDelete }: EntryTableProps) {
   const loader = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
+    console.log("Pages loaded:", data?.pages.length);
+    console.log("Has next page:", hasNextPage);
     if (!loader.current || !hasNextPage) return;
+
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting) fetchNextPage();
       },
       { threshold: 1 }
     );
+
     observer.observe(loader.current);
     return () => observer.disconnect();
   }, [fetchNextPage, hasNextPage]);
 
   return (
     <div className="overflow-x-auto">
-      <table className="min-w-full bg-white shadow rounded">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Title</th>
-            <th className="px-4 py-2">Type</th>
-            <th className="px-4 py-2">Director</th>
-            <th className="px-4 py-2">Budget</th>
-            <th className="px-4 py-2">Location</th>
-            <th className="px-4 py-2">Duration</th>
-            <th className="px-4 py-2">Year</th>
-            <th className="px-4 py-2">Created At</th>
-            <th className="px-4 py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.pages.flat().map((entry: Entry) => (
-            <tr key={entry.id}>
-              <td className="border px-4 py-2">{entry.title}</td>
-              <td className="border px-4 py-2">{entry.type}</td>
-              <td className="border px-4 py-2">{entry.director}</td>
-              <td className="border px-4 py-2">{entry.budget}</td>
-              <td className="border px-4 py-2">{entry.location}</td>
-              <td className="border px-4 py-2">{entry.duration}</td>
-              <td className="border px-4 py-2">{entry.year}</td>
-              <td className="border px-4 py-2">
-                {new Date(entry.createdAt).toLocaleString("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </td>
-              <td className="border px-4 py-2">
-                <button
-                  className="bg-yellow-500 text-white px-2 py-1 rounded mr-2"
-                  onClick={() => onEdit(entry)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="bg-red-600 text-white px-2 py-1 rounded"
-                  onClick={() => onDelete(entry)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead>Type</TableHead>
+            <TableHead>Director</TableHead>
+            <TableHead>Budget</TableHead>
+            <TableHead>Location</TableHead>
+            <TableHead>Duration</TableHead>
+            <TableHead>Year</TableHead>
+            <TableHead>Created At</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {data?.pages
+            .flat()
+            .map(
+              ({
+                id,
+                title,
+                type,
+                director,
+                budget,
+                location,
+                duration,
+                year,
+                createdAt,
+              }: Entry) => (
+                <TableRow key={id}>
+                  <TableCell>{title}</TableCell>
+                  <TableCell>{type}</TableCell>
+                  <TableCell>{director}</TableCell>
+                  <TableCell>{budget}</TableCell>
+                  <TableCell>{location}</TableCell>
+                  <TableCell>{duration}</TableCell>
+                  <TableCell>{year}</TableCell>
+                  <TableCell>
+                    {new Date(createdAt).toLocaleString("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </TableCell>
+                  <TableCell className="flex gap-2">
+                    <Button
+                      className="cursor-pointer"
+                      variant="secondary"
+                      onClick={() =>
+                        onEdit({
+                          id,
+                          title,
+                          type,
+                          director,
+                          budget,
+                          location,
+                          duration,
+                          year,
+                          createdAt,
+                        })
+                      }
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      className="cursor-pointer"
+                      variant="destructive"
+                      onClick={() =>
+                        onDelete({
+                          id,
+                          title,
+                          type,
+                          director,
+                          budget,
+                          location,
+                          duration,
+                          year,
+                          createdAt,
+                        })
+                      }
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            )}
+        </TableBody>
+      </Table>
+
       <div ref={loader} className="h-8 flex items-center justify-center">
         {isFetchingNextPage && <span>Loading more...</span>}
       </div>
